0c510c9c9375fe84bf064d03dca09b5b557b4bde
guilherme1s <2000guilherme.silva@gmail.com>
2026-03-29 10:39:29 -0300

    [ADD] Configuração do api client

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,17 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { ThemeProvider } from "./components/ui/theme/theme-provider";
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 export function App() {
   return (
     <HelmetProvider>
       <ThemeProvider storageKey="myfinanceflow.theme" defaultTheme="light">
         <Helmet titleTemplate="%s | MyFinanceFlow" />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   );
